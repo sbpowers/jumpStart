45a41b558eda2c3847595fb72f72dc40e54a8bcf
Steve Powers <sbpowers@gmail.com>
2017-05-01 11:16:21 -0700

    jumpstart working

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,6 +9,19 @@ $(document).ready(function() {
     pauseOnHover: false,
     pauseOnFocus: false
   });
+
+  var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrollTop = scrollTop;
+    }
+  });
 });
 
 
