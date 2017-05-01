bdf2c9bf4d5e3c89cfcbb64e63a853b0fa5a6abf
Steve Powers <sbpowers@gmail.com>
2017-05-01 10:57:47 -0700

    carousel basically working

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
 // More jQuery code goes in here later
+
+  $('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnHover: false,
+    pauseOnFocus: false
+  });
 });
+
+
+//The carousel should autoplay when the page is loaded.
+//The speed should be 4000 milliseconds.
+//Do not include arrow buttons.
+//Do not allow the user to drag the images.
+//Do not pause on "focus" or "hover".
